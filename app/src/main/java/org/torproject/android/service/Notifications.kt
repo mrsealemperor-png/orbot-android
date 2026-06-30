e55c170485fc8df116b36a6e99ad20c60ed52b1c
mrsealemperor-png <mrsealemperor@gmail.com>
2026-06-30 18:38:39 +0300

    Update Notifications.kt

--- app/src/main/java/org/torproject/android/service/Notifications.kt
+++ app/src/main/java/org/torproject/android/service/Notifications.kt
@@ -7,7 +7,7 @@ import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import org.torproject.android.R
+import com.seal.tyulenvpn.R
 import org.torproject.android.util.Prefs
 import org.torproject.jni.TorService
 
